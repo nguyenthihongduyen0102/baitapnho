9e39a34b17c153ca2efe55307a11ee040015ee65
Nguyen Thi Hong Duyen <251020910645@st.phenikaa-uni.edu.vn>
2026-08-11 19:53:46 +0700

    sửa lại phần list...

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -12,13 +12,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        KhoaHocTuHoc th1 = new KhoaHocTuHoc(1,
+        List<KhoaHoc> dsKhoaHoc = new ArrayList<>();
+        try{
+            KhoaHocTuHoc th1 = new KhoaHocTuHoc(1,
                 "Tu hoc",
                 "com ga chien",
                 800,
                 2
         );
-        KhoaHocMentor mt1 = new KhoaHocMentor(
+            dsKhoaHoc.add(th1);
+            th1.setSoThangDK(-1);
+        } catch(IllegalArgumentException e){
+            System.out.println("Loi: " + e.getMessage());
+        }
+
+        try {
+            KhoaHocMentor mt1 = new KhoaHocMentor(
                 2,
                 "Mentor",
                 "My cay",
@@ -26,7 +35,14 @@ public class Main {
                 5,
                 200
         );
-        KhoaHocDoanhNghiep dn2 = new KhoaHocDoanhNghiep(
+            dsKhoaHoc.add(mt1);
+            mt1.setSoBuoiMentor(0);
+        } catch(IllegalArgumentException e){
+            System.out.println("Loi: " + e.getMessage());
+        }
+
+        try{
+            KhoaHocDoanhNghiep dn2 = new KhoaHocDoanhNghiep(
                 3,
                 "Doanh nghiep",
                 "Sua tuoi",
@@ -34,43 +50,22 @@ public class Main {
                 12,
                 2
         );
-        KhoaHocTuHoc th2 = new KhoaHocTuHoc(
-                4,
-                "Thu Vien",
-                "Bun cha",
-                400,
-                2
-        );
-        List<KhoaHoc> dsKhoaHoc = new ArrayList<>();
-        dsKhoaHoc.add(th1);
-        dsKhoaHoc.add(mt1);
-        dsKhoaHoc.add(dn2);
-        dsKhoaHoc.add(th2);
-//        try{
-//            th1.setSoThangDK(-1);
-//        } catch(IllegalArgumentException e){
-//            System.out.println("Da nhap sai thong tin");
-//        }
-//
-//        try {
-//            mt1.setSoBuoiMentor(0);
-//        } catch(IllegalArgumentException e){
-//            System.out.println("Da nhap sai thong tin");
-//        }
-//
-//        try{
-//            dn2.setMucChietKhau(-1);
-//        } catch(IllegalArgumentException e){
-//            System.out.println("Da nhap sai thong tin");
-//        }
+            dsKhoaHoc.add(dn2);
+            dn2.setMucChietKhau(-1);
+        } catch(IllegalArgumentException e){
+            System.out.println("Loi: " + e.getMessage());
+        }
+
         for(int i=0;i<dsKhoaHoc.size();i++){
             dsKhoaHoc.get(i).hienThi();
         }
+
         Comparator<KhoaHoc> tonghocphi = new Comparator<KhoaHoc>() {
             public int compare(KhoaHoc k1, KhoaHoc k2) {
                 return Double.compare(k1.tinhHocPhi(), k2.tinhHocPhi());
             }
         };
+
         Collections.sort(dsKhoaHoc, tonghocphi);
         System.out.println("--------------------------------");
         for(int i=0;i<dsKhoaHoc.size();i++){
